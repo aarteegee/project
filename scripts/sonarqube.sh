633c900f4dff7ca0198f1d974d39f754f1937c6c
Ravi Gottipati <60428064+ravigottipati92@users.noreply.github.com>
2020-03-21 18:48:07 -0700

    sonar installation script

---
+++ scripts/sonarqube.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+wget https://repo1.maven.org/maven2/org/codehaus/sonar/runner/sonar-runner-dist/2.4/sonar-runner-dist-2.4.zip
+unzip sonar-runner-dist-2.4.zip
+mv sonar-runner-2.4 /opt
+exit
